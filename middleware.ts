85770fc7adbdc3f3a0522625c5adb00b290715a1
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-09-21 00:00:58 +0530

    signin and signup route middleware protection

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,20 @@ import { toast } from "sonner";
 export function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken")?.value;
 
+  const protectedRoutes = ["/signin", "/signup"];
+
   if (!accessToken) {
     toast.message("Please sign in again");
     return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  if (accessToken && protectedRoutes.includes(req.nextUrl.pathname)) { 
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/signin", "/signup"],
 };
