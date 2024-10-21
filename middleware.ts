1be19b83a6fffa8e416f44b4524e849eca1b6b07
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-10-22 02:11:41 +0530

    update middleware temporary

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken");
 
-  const protectedRoutes = ["/signin", "/signup"];
+  // const protectedRoutes = ["/signin", "/signup"];
 
-  if (!accessToken && ["/dashboard"].includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL("/signin", req.url));
-  }
+  // if (!accessToken && ["/dashboard"].includes(req.nextUrl.pathname)) {
+  //   return NextResponse.redirect(new URL("/signin", req.url));
+  // }
 
-  if (accessToken && protectedRoutes.includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+  // if (accessToken && protectedRoutes.includes(req.nextUrl.pathname)) {
+  //   return NextResponse.redirect(new URL("/dashboard", req.url));
+  // }
 
   // console.log("Proceeding with the request");
   return NextResponse.next();
